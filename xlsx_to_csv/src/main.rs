5be8c443180471844f83bf477316fa3ae5b8e695
Yudi <12040397+Yudi@users.noreply.github.com>
2024-11-24 13:06:31 -0300

    Fix dates not being preserved

--- xlsx_to_csv/src/main.rs
+++ xlsx_to_csv/src/main.rs
@@ -1,4 +1,5 @@
-use calamine::{open_workbook, Reader, Xlsx};
+use calamine::{open_workbook, DataType, Reader, Xlsx};
+use chrono::NaiveDate;
 use std::error::Error;
 use std::path::Path;
 
@@ -39,7 +40,47 @@ fn main() -> Result<(), Box<dyn Error>> {
     for row in range.rows() {
         let row_vec: Vec<String> = row
             .iter()
-            .map(|cell: &calamine::DataType| cell.to_string())
+            .map(|cell: &DataType| match cell {
+                DataType::DateTime(days) => {
+                    let days_integer_part: i64 = *days as i64;
+                    let days_fractional_part: f64 = *days - days_integer_part as f64;
+
+                    if days_integer_part == 0 && days_fractional_part > 0.0 {
+                        // Handle time-only values
+                        let total_seconds = (days_fractional_part * 86400.0).round() as u32;
+                        let hours: u32 = total_seconds / 3600;
+                        let minutes: u32 = (total_seconds % 3600) / 60;
+                        let seconds: u32 = total_seconds % 60;
+                        format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+                    } else {
+                        // Handle date or datetime values
+                        if let Some(date) =
+                            NaiveDate::from_ymd_opt(1899, 12, 30).and_then(|d: NaiveDate| {
+                                d.checked_add_signed(chrono::Duration::days(days_integer_part))
+                            })
+                        {
+                            if days_fractional_part == 0.0 {
+                                date.format("%Y/%m/%d").to_string()
+                            } else {
+                                let total_seconds = (days_fractional_part * 86400.0).round() as u32;
+                                let hours: u32 = total_seconds / 3600;
+                                let minutes: u32 = (total_seconds % 3600) / 60;
+                                let seconds: u32 = total_seconds % 60;
+                                format!(
+                                    "{} {:02}:{:02}:{:02}",
+                                    date.format("%Y/%m/%d"),
+                                    hours,
+                                    minutes,
+                                    seconds
+                                )
+                            }
+                        } else {
+                            cell.to_string()
+                        }
+                    }
+                }
+                _ => cell.to_string(),
+            })
             .collect();
         wtr.write_record(&row_vec)?;
     }
