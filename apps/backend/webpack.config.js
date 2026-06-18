95432a76f97d558eeb4b566aaf338cfd9e1f3545
Yudi <12040397+Yudi@users.noreply.github.com>
2026-06-18 03:11:01 -0300

    Update webpack.config.js

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -1,6 +1,20 @@
 const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
+class BackendTypeScriptLoaderPlugin {
+  apply(compiler) {
+    const tsLoaderRule = compiler.options.module?.rules?.find((rule) =>
+      String(rule?.loader ?? '').includes('ts-loader'),
+    );
+
+    if (!tsLoaderRule?.options) {
+      return;
+    }
+
+    tsLoaderRule.options.transpileOnly = false;
+  }
+}
+
 module.exports = {
   output: {
     path: join(__dirname, '../../dist/apps/backend'),
@@ -18,7 +32,9 @@ module.exports = {
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
+      typeCheckOptions: false,
       sourceMaps: true,
     }),
+    new BackendTypeScriptLoaderPlugin(),
   ],
 };
